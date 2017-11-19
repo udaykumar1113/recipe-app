cb93cd8872f7f582303c5b9bbbfb24691d759a7a
uday <udaykumar1113@gmail.com>
2017-11-19 13:52:06 -0500

    added unit of measure class with fetch type

--- src/main/java/com/recipe/domain/Ingredient.java
+++ src/main/java/com/recipe/domain/Ingredient.java
@@ -2,10 +2,12 @@ package com.recipe.domain;
 
 import java.math.BigDecimal;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Ingredient {
@@ -20,6 +22,9 @@ public class Ingredient {
 	
 	@ManyToOne
 	private Recipe recipe;
+	
+	@OneToOne(fetch=FetchType.EAGER)
+	private UnitOfMeasure unitOfMeasure;
 
 	public Long getId() {
 		return id;
