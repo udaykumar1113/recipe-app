aa91b8e26e76eaed3248e159a3b75c5184ea2f32
uday <udaykumar1113@gmail.com>
2017-11-19 16:05:23 -0500

    added difficulty enumeration

---
+++ src/main/java/com/recipe/domain/Difficulty.java
@@ -0,0 +1,6 @@
+package com.recipe.domain;
+
+public enum Difficulty {
+	
+	EASY, MODERATE, HARD
+}
--- src/main/java/com/recipe/domain/Recipe.java
+++ src/main/java/com/recipe/domain/Recipe.java
@@ -4,6 +4,8 @@ import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -27,7 +29,9 @@ public class Recipe {
 	
 	@Lob
 	private byte[] image;
-	//private Difficulty difficulty;
+	
+	@Enumerated(value=EnumType.STRING)
+	private Difficulty difficulty;
 	
 	@OneToOne(cascade=CascadeType.ALL)
 	private Notes notes;
@@ -35,6 +39,22 @@ public class Recipe {
 	@OneToMany(cascade=CascadeType.ALL, mappedBy="recipe")
 	private Set<Ingredient> ingrident;
 	
+	public Difficulty getDifficulty() {
+		return difficulty;
+	}
+
+	public void setDifficulty(Difficulty difficulty) {
+		this.difficulty = difficulty;
+	}
+
+	public Set<Ingredient> getIngrident() {
+		return ingrident;
+	}
+
+	public void setIngrident(Set<Ingredient> ingrident) {
+		this.ingrident = ingrident;
+	}
+
 	public Long getId() {
 		return id;
 	}
