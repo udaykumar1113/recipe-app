b41076a88997a7b58578e8e25a655b457a948c3d
uday <udaykumar1113@gmail.com>
2017-11-19 13:35:33 -0500

    added one to many bidirectional relationship on Recipe and ingredient, corrected ingrident class name

--- src/main/java/com/recipe/domain/Recipe.java
+++ src/main/java/com/recipe/domain/Recipe.java
@@ -1,11 +1,14 @@
 package com.recipe.domain;
 
+import java.util.Set;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -28,6 +31,9 @@ public class Recipe {
 	
 	@OneToOne(cascade=CascadeType.ALL)
 	private Notes notes;
+	
+	@OneToMany(cascade=CascadeType.ALL, mappedBy="recipe")
+	private Set<Ingredient> ingrident;
 	
 	public Long getId() {
 		return id;
